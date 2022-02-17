2423a8182650156c5602535609ecde8c0031d182
Evgeniy <e.valerevich@gmail.com>
2022-02-18 00:11:04 +0300

    init: start the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
+  var dosen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+  let teen = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty']
+
+    if (number<=10)
+      return dosen[number]
+    else if (number<=20)
+      return teen[number]
   
 }
